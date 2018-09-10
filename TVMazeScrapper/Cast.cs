cc682702b93194ca8ee21aa244c690b38ee4892c
PAL\AnKozlov <Andrey.Kozlov@broadridge.com>
2018-09-10 17:43:02 +0300

    init

---
+++ TVMazeScrapper/Cast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TVMazeScrapper
+{
+    public class Cast
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+}
+
---
+++ TVMazeScrapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace TVMazeScrapper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string str = await client.GetStringAsync("http://api.tvmaze.com/shows/");
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+            string[] lines = await File.ReadAllLinesAsync("tvmazecast.json");
+            
+
+
+            client.Dispose();
+            
+        }
+
+
+    }
+}
